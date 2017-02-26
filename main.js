a41c22d9453bfa22ac109c38e515058f00462527
Alicia Miceli <akb.miceli@gmail.com>
2017-02-26 11:44:23 -0600

    working slideshow

---
+++ main.js
@@ -0,0 +1,77 @@
+myFunction();
+
+setInterval(function () {
+myFunction();
+}, 28000);
+
+function myFunction(){
+
+$('#img1').show();
+
+setTimeout(function () {
+
+	$('#img1').fadeOut();
+}, 4000);
+
+$('#img2').hide();
+
+ setTimeout(function() {
+ 	$('#img2').fadeIn();
+ }, 0030);
+
+setTimeout(function () {
+	$('#img2').fadeOut();
+}, 8000);
+
+$('#img3').hide();
+
+ setTimeout(function() {
+ 	$('#img3').fadeIn();
+ }, 0030);
+
+setTimeout(function () {
+	$('#img3').fadeOut();
+}, 12000);
+
+$('#img4').hide();
+
+ setTimeout(function() {
+ 	$('#img4').fadeIn();
+ }, 0030);
+
+setTimeout(function () {
+	$('#img4').fadeOut();
+}, 16000);
+
+$('#img5').hide();
+
+ setTimeout(function() {
+ 	$('#img5').fadeIn();
+ }, 0030);
+
+setTimeout(function () {
+	$('#img5').fadeOut();
+}, 20000);
+
+$('#img6').hide();
+
+ setTimeout(function() {
+ 	$('#img6').fadeIn();
+ }, 0030);
+
+setTimeout(function () {
+	$('#img6').fadeOut();
+}, 24000);
+
+$('#img7').hide();
+
+ setTimeout(function() {
+ 	$('#img7').fadeIn();
+ }, 0030);
+
+setTimeout(function () {
+	$('#img7').fadeOut();
+}, 28000);
+
+};
+
